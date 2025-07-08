ca1464743e3c53f1d5b1d835ddced8e4fc742745
Ravikumar <ravikumar936318@gmail.com>
2025-07-08 11:41:11 +0530

    change

--- src/main/java/com/example/NurseryProject/Controll/Controller.java
+++ src/main/java/com/example/NurseryProject/Controll/Controller.java
@@ -14,7 +14,7 @@ import com.example.NurseryProject.Repo.Repository;
 import com.example.NurseryProject.Service.DataService;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:5500","http://127.0.0.1:5500","http://localhost:5173/","http://localhost:5174/"})
+@CrossOrigin(origins = {"https://nurseryadmin.netlify.app/","https://nurseryuser.netlify.app/"})
 public class Controller {
 	@Autowired
 	private Repository repository;
